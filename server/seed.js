670eb9306718d1a3ffa6b63babdcc4dfbc4fca93
Nitya022004 <nitya.2004cse@gmail.com>
2025-06-01 12:02:32 +0000

    Final Docker + Jenkins + CI/CD setup with working frontend/backend

--- server/seed.js
+++ server/seed.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 const Teacher = require('./models/Teacher');
 const StudentReport = require('./models/StudentReport');
 
-mongoose.connect('mongodb://localhost:27017/gradescope', {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ app.use('/api/students', studentRoutes);
 
 
 // Connect to DB
-mongoose.connect('mongodb://localhost:27017/gradescope', {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
